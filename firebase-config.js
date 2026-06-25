62b2400816214089883c4582c763fb31bd2f9182
minimi98 <mini.siehlow@googlemail.com>
2026-06-25 15:54:30 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAXKhycj1sy8VO_fOn4RcKHIMmFDj93YO4",
-  authDomain: "oritt-beachplease.firebaseapp.com",
-  projectId: "oritt-beachplease",
-  storageBucket: "oritt-beachplease.firebasestorage.app",
-  messagingSenderId: "585945520778",
-  appId: "1:585945520778:web:a53cf26fbd8344988f2b73"
-};
