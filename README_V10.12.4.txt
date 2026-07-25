b08dbcf743cc046f22146bdbf7e546baaa6cab0e
minimi98 <mini.siehlow@googlemail.com>
2026-07-25 06:22:19 +0200

    V10.12.4

---
+++ README_V10.12.4.txt
@@ -0,0 +1,10 @@
+RVN Event Manager V10.12.4 – Abmeldebutton im Helferbereich
+
+Änderungen:
+- Abmeldebutton an der eigenen Station bleibt erhalten.
+- Abmeldebutton zusätzlich im Meldestellenbereich für angemeldete Helfer.
+- Abmeldebutton zusätzlich im Springer-/Meldungsbereich.
+- Beim Abmelden werden Helfer-ID, Telefonnummer, Stationszugang und Helferpasswort-Sitzung vollständig gelöscht.
+- Danach erscheint wieder die Passwortabfrage des Helferbereichs.
+- Admin-Abmeldung und Teilnehmer-Abmeldung bleiben unverändert.
+- Cache-Version auf V10.12.4 erhöht.
--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
-const CACHE = "rvn-v10-12-3";
+const CACHE = "rvn-v10-12-4";
 const CORE = [
   "./",
   "./index.html",
-  "./css/style.css?v=10.12.3",
-  "./js/app.js?v=10.12.3",
-  "./js/views.js?v=10.12.3",
-  "./js/state.js?v=10.12.3",
-  "./js/utils.js?v=10.12.3",
-  "./js/firebase.js?v=10.12.3",
-  "./firebase-config.js?v=10.12.3",
+  "./css/style.css?v=10.12.4",
+  "./js/app.js?v=10.12.4",
+  "./js/views.js?v=10.12.4",
+  "./js/state.js?v=10.12.4",
+  "./js/utils.js?v=10.12.4",
+  "./js/firebase.js?v=10.12.4",
+  "./firebase-config.js?v=10.12.4",
   "./assets/logo.png",
   "./assets/logo.jpg",
   "./assets/beach-poster.png",
